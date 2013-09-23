606b6384e4e969aa9c5b1c7f4608c1815d2bea85
pianopiano <mail.pianopiano@gmail.com>
2013-09-23 15:10:24 +0900

    宇宙

---
+++ javascripts/index.js
@@ -0,0 +1,51 @@
+var project;
+(function(project) {
+	var Main = (function() {
+		function Main() {
+			var _this = this;
+			away.Debug.THROW_ERRORS = false;
+			away.Debug.LOG_PI_ERRORS = false;
+			away.Debug.ENABLE_LOG = false;
+			this.view = new away.containers.View3D();
+			this.view.backgroundColor = 0x121212;
+			this.view.antiAlias = 1;
+			this.container = new away.containers.ObjectContainer3D();
+			this.view.scene.addChild(this.container);
+			var light = new away.lights.PointLight();
+			light.ambient = 10;
+			light.specular = 10;
+			light.color = 0xFFFFFF;
+			var geometry = new away.primitives.CubeGeometry(1, 1, 1);
+			for (var i = 0; i < 500; i++) {
+				var matTx = new away.materials.ColorMaterial(Math.random() * 0xFF00FF);
+				matTx.lightPicker = new away.materials.StaticLightPicker([light]);
+				var mesh = new away.entities.Mesh(geometry, matTx);
+				mesh.x = 1500 * (Math.random() - 0.5);
+				mesh.y = 1000 * (Math.random() - 0.5);
+				mesh.z = 1000 * (Math.random() - 0.5);
+				mesh.rotationX = mesh.rotationY = mesh.rotationZ = i
+				this.container.addChild(mesh);
+			}
+			this.raf = new away.utils.RequestAnimationFrame(this.render, this);
+			this.raf.start();
+			this.handleResize();
+			window.onresize = function() {
+				_this.handleResize();
+			};
+		}
+		Main.prototype.render = function(e) {
+			this.container.rotationX += 0.1;
+			this.container.rotationY += 0.1;
+			this.view.render();
+		};
+		Main.prototype.handleResize = function() {
+			this.view.width = window.innerWidth;
+			this.view.height = window.innerHeight;
+		};
+		return Main;
+	})();
+	project.Main = Main;
+})(project || (project = {}));
+window.onload = function() {
+	new project.Main();
+};
